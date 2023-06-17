6da7e80fbcd50900c231baae9d61b8fe0902d124
神楽坂忻 <38414925+xin0aini@users.noreply.github.com>
2023-06-17 11:07:01 +0800

    Add files via upload

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -383,9 +383,6 @@ static int input_get_disposition(struct input_dev *dev,
 extern void __attribute__((weak)) oplus_sync_saupwk_event(unsigned int , unsigned int , int);
 #endif /* OPLUS_FEATURE_SAUPWK */
 
-extern bool ksu_input_hook __read_mostly;
-extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
-
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
@@ -396,8 +393,6 @@ static void input_handle_event(struct input_dev *dev,
         oplus_sync_saupwk_event(type, code, value);
 #endif /* OPLUS_FEATURE_SAUPWK */
 
-    if (unlikely(ksu_input_hook))
-        ksu_handle_input_handle_event(&type, &code, &value);
 	if (disposition != INPUT_IGNORE_EVENT && type != EV_SYN)
 		add_input_randomness(type, code, value);
 
